b113f61d8917cfefa9c8b6c7c14e0b4e9ae6db2b
unknown <yofran.garnier03@gmail.com>
2023-03-03 13:21:16 -0400

    second commit

--- src/pages/api/data.js
+++ src/pages/api/data.js
@@ -5,7 +5,7 @@ export const projects = [
       title: "My City Dogs",
       Description:
         "This project is an individual work that was created for the Henry Bootcamp. It utilizes the DogsApi and uses PostgreSQL as the database. The backend was developed with Node.JS and Express, and the frontend was created with React, Redux, and pure CSS.",
-        image: "/assets/dogs.png",
+        image: "/assets/dogs.jpg",
       GitHub: ["https://github.com/yofrangarnier/Dogs-Pi/tree/main/PI-Dogs-main"],
       
     },
@@ -14,7 +14,7 @@ export const projects = [
       title: "ACMEtronics",
       Description:
         "This project was an e-commerce platform created as a group effort for the Henry Bootcamp. We created our own information to use as an API and worked with PostgreSQL as the database. The backend was developed with Node.js and Express, and the frontend was created with React, Redux, Bootstrap, and Material UI.",
-      image: "/assets/acmetronic.png",
+      image: "/assets/acmetronic.jpg",
       GitHub: [
         "https://github.com/kachamozo/ACMEtronics-back",
         "https://github.com/kachamozo/ACMEtronics-front",
